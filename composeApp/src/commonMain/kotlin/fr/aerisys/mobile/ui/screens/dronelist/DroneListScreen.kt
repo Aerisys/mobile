f542aa4ea8ea811f79e2060516d436cdde9ddbf6
Loan <loanfrancois1@gmail.com>
2025-11-06 16:53:52 +0100

    fix: switch to theme color

--- composeApp/src/commonMain/kotlin/fr/aerisys/mobile/ui/screens/dronelist/DroneListScreen.kt
+++ composeApp/src/commonMain/kotlin/fr/aerisys/mobile/ui/screens/dronelist/DroneListScreen.kt
@@ -4,7 +4,6 @@ import aerisys.composeapp.generated.resources.Res
 import aerisys.composeapp.generated.resources.en_name
 import aerisys.composeapp.generated.resources.en_ok
 import aerisys.composeapp.generated.resources.en_status
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,7 +20,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -30,14 +28,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import fr.aerisys.mobile.ui.screens.components.BottomNavigationBar
 import fr.aerisys.mobile.ui.viewmodel.MainViewModel
@@ -52,7 +48,6 @@ fun DroneListScreen(
     val drones = viewModel.drones
 
     Scaffold(
-        containerColor = Color(0x1E1E1E),
         topBar = {
             TopAppBar(
                 title = { Text("Drones list", color = Color.White) },
@@ -66,9 +61,6 @@ fun DroneListScreen(
                         Icon(Icons.Default.Add, contentDescription = "Add", tint = Color.White)
                     }
                 },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color(0xFF2A2A2A)
-                )
             )
         },
         bottomBar = {
@@ -78,14 +70,12 @@ fun DroneListScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(padding)
-                .background(Color(0xFF1E1E1E)),
+                .padding(padding),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(Modifier.height(20.dp))
 
             Card(
-                colors = CardDefaults.cardColors(containerColor = Color(0xFF2C2C2C)),
                 modifier = Modifier
                     .padding(16.dp)
                     .fillMaxWidth()
@@ -103,12 +93,12 @@ fun DroneListScreen(
                             .padding(bottom = 8.dp),
                         horizontalArrangement = Arrangement.SpaceBetween
                     ) {
-                        Text(stringResource(Res.string.en_name), color = Color.Gray, fontSize = 14.sp)
-                        Text(stringResource(Res.string.en_status), color = Color.Gray, fontSize = 14.sp)
+                        Text(stringResource(Res.string.en_name))
+                        Text(stringResource(Res.string.en_status))
                         Spacer(Modifier.width(24.dp))
                     }
 
-                    HorizontalDivider(Modifier, 1.dp, Color.DarkGray)
+                    HorizontalDivider(Modifier, 1.dp)
 
                     LazyColumn {
                         items(drones) { drone ->
@@ -119,11 +109,11 @@ fun DroneListScreen(
                                 horizontalArrangement = Arrangement.SpaceBetween,
                                 verticalAlignment = Alignment.CenterVertically
                             ) {
-                                Text(drone.name, color = Color.White, fontWeight = FontWeight.SemiBold)
-                                Text(stringResource(Res.string.en_ok), color = Color(0xFF3DDC84))
+                                Text(drone.name, fontWeight = FontWeight.SemiBold)
+                                Text(stringResource(Res.string.en_ok), color = Color.Green)
 
                             }
-                            HorizontalDivider(Modifier,0.5.dp, Color.DarkGray)
+                            HorizontalDivider(Modifier,0.5.dp)
                         }
                     }
                 }
