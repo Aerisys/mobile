1fa860a353ee0837959900c096c0b8825f53e598
Loan <loanfrancois1@gmail.com>
2025-11-06 16:44:26 +0100

    fix: imports

--- composeApp/src/commonMain/kotlin/fr/aerisys/mobile/ui/Navigation.kt
+++ composeApp/src/commonMain/kotlin/fr/aerisys/mobile/ui/Navigation.kt
@@ -4,9 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import fr.aerisys.mobile.ui.screens.home.HomeScreen
 import fr.aerisys.mobile.ui.screens.listdrone.ListDroneScreen
-import fr.aerisys.mobile.ui.screens.Home.HomeScreen
-import fr.aerisys.mobile.ui.screens.ListDrone.ListDroneScreen
 import kotlinx.serialization.Serializable
 
 class Routes {
