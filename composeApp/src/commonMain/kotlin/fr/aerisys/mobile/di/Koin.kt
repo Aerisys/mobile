8599832af781c19ae780bcbdd916d0bfd2b1a1ab
AmauryRDV <97154136+AmauryRDV@users.noreply.github.com>
2025-11-06 15:22:40 +0100

    feat: database init

--- composeApp/src/commonMain/kotlin/fr/aerisys/mobile/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/fr/aerisys/mobile/di/Koin.kt
@@ -1,12 +1,14 @@
 package fr.aerisys.mobile.di
 
 import org.koin.core.context.startKoin
+import org.koin.core.module.Module
 import org.koin.dsl.KoinAppDeclaration
 
+expect fun databaseModule(): Module
 fun initKoin(appDeclaration: KoinAppDeclaration = {}) =
     startKoin {
         appDeclaration()
-        modules()
+        modules(databaseModule())
     }
 
 fun initKoin() = initKoin {}
