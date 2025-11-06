f4431ce38d78af89a24599f249255491f6559e30
Loan <lfrancois@velan.fr>
2025-11-06 12:54:52 +0100

    add SQLDelight

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     kotlin("plugin.serialization") version "2.0.0"
+    id("app.cash.sqldelight") version "2.1.0"
 }
 
 kotlin {
@@ -22,6 +23,7 @@ kotlin {
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
             isStatic = true
+            linkerOpts("-lsqlite3")
         }
     }
     
@@ -30,6 +32,7 @@ kotlin {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
             implementation("io.insert-koin:koin-android:4.1.+")
+            implementation("app.cash.sqldelight:android-driver:2.1.0")
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -40,14 +43,27 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
-            implementation("androidx.navigation:navigation-compose:2.9.+")
+            implementation("org.jetbrains.androidx.navigation:navigation-compose:2.9.+")
             implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.+")
             implementation("io.insert-koin:koin-compose:4.1.+")
             implementation("io.insert-koin:koin-compose-viewmodel:4.1.+")
             implementation("io.insert-koin:koin-compose-viewmodel-navigation:4.1.+")
+            implementation("app.cash.sqldelight:runtime:2.1.0")
+            implementation("app.cash.sqldelight:coroutines-extensions:2.1.0")
+        }
+        iosMain.dependencies {
+            implementation("app.cash.sqldelight:native-driver:2.1.0")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+        }
+    }
+}
+
+sqldelight {
+    databases {
+        create("AerisysDatabase") {
+            packageName.set("fr.aerisys.mobile.db")
         }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/fr/aerisys/mobile/model/DataBase.kt
@@ -0,0 +1,5 @@
+package fr.aerisys.mobile.model
+
+import org.koin.core.module.Module
+
+expect fun databaseModule(): Module
