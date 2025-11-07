d9c3df56683951e63e9a69285e333e5d05c24112
maxence <maxence.chachane@gmail.com>
2025-11-07 09:10:31 +0100

    fix: bdd add ios

---
+++ composeApp/src/iosMain/kotlin/fr/aerisys/mobile/di/Koin.ios.kt
@@ -0,0 +1,13 @@
+package fr.aerisys.mobile.di
+
+import app.cash.sqldelight.driver.native.NativeSqliteDriver
+import fr.aerisys.mobile.db.AerisysDatabase
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+actual fun databaseModule() = module {
+    single {
+        val driver = NativeSqliteDriver(AerisysDatabase.Schema, "AerisysDatabase.db")
+        AerisysDatabase(driver)
+    }
+}
