6209a74c531b367346c23f0ca37d5bd1c8eda332
MAXCOEUR <45947596+MAXCOEUR@users.noreply.github.com>
2025-11-07 08:49:09 +0100

    Camera (#8)
    * add SQLDelight
    
    * feat: database init
    
    * feat: tables split
    
    * feat: Database updated + MainViewModel
    
    ---------
    
    Co-authored-by: Loan <lfrancois@velan.fr>
    Co-authored-by: sachabarbet <sacha.barbet@proton.me>
    Co-authored-by: AmauryRDV <97154136+AmauryRDV@users.noreply.github.com>

--- composeApp/src/commonMain/kotlin/fr/aerisys/mobile/App.kt
+++ composeApp/src/commonMain/kotlin/fr/aerisys/mobile/App.kt
@@ -3,12 +3,16 @@ package fr.aerisys.mobile
 import androidx.compose.runtime.Composable
 import fr.aerisys.mobile.ui.AppNavigation
 import fr.aerisys.mobile.ui.theme.AppTheme
+import fr.aerisys.mobile.viewModel.MainViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.koinInject
 
 @Composable
 @Preview
 fun App() {
     AppTheme {
-        AppNavigation()
+        val mainViewModel: MainViewModel = koinInject()
+        AppNavigation(mainViewModel = mainViewModel)
     }
+
 }
