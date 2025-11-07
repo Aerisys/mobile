10c38c4e1b9fdc4763a2c8e341ab17fc30fb60b9
Philibert <gael.p40@outlook.fr>
2025-11-07 15:21:43 +0100

    ajout de Sansnom.glb update de Drone.kt ajout des colonnes

--- composeApp/src/commonMain/kotlin/fr/aerisys/mobile/model/Drone.kt
+++ composeApp/src/commonMain/kotlin/fr/aerisys/mobile/model/Drone.kt
@@ -2,13 +2,13 @@ package fr.aerisys.mobile.model
 
 
 data class Drone(
-    val id: Long,
-    val user_id: Long,
-    val name: String,
-    val mac_adress: String,
-    val ip_adress: String,
-    val flight_mode: String,
-    val motor_power: Int,
-    val altitude_limit: Int,
-    val added_at: String
+    val id: Long =0,
+    val user_id: Long = 0,
+    val name: String = "null",
+    val mac_adress: String = "null",
+    val ip_adress: String = "null",
+    val flight_mode: String = "null",
+    val motor_power: Int = 0,
+    val altitude_limit: Int = 0,
+    val added_at: String = "null"
 )
