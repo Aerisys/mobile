9e0a6a38ab6f93b6ee385f36176d2f4ef99c2fc3
Loan <lfrancois@velan.fr>
2025-12-15 15:40:10 +0100

    feat: firebase auth

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,15 +1,45 @@
 import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../presentation/views/contact_page.dart';
 import '../../presentation/views/home_page.dart';
+import '../../presentation/views/login_page.dart';
 import 'app_routes.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: AppRoutes.home,
+  initialLocation: AppRoutes.login,
+  redirect: (context, state) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+    
+    final String location = state.uri.toString();
+    final bool isGoingToLogin = location == AppRoutes.login;
+    
+    if (!isLoggedIn && !isGoingToLogin) {
+    return AppRoutes.login;
+    }
+    
+    if (isLoggedIn && isGoingToLogin) {
+    return AppRoutes.home;
+    }
+    
+    return null;
+  },
   routes: [
+    GoRoute(
+      path: AppRoutes.login,
+      name: 'login',
+      builder: (context, state) => const LoginPage(),
+    ),
     GoRoute(
       path: AppRoutes.home,
       name: 'home',
       builder: (context, state) => const HomePage(),
+    ),
+    GoRoute(
+      path: AppRoutes.contact,
+      name: 'contact',
+      builder: (context, state) => const ContactPage(),
     ),
   ],
 );
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart'; // 1. Import Firebase Core
 
 import 'core/routes/app_router.dart';
 import 'core/themes/app_theme.dart';
+import 'firebase_options.dart';
+import 'presentation/view_model/login_view_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(const HereBro());
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => LoginViewModel(),
+      child: const HereBro(),
+    ),
+  );
 }
 
 class HereBro extends StatelessWidget {
