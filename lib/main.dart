c55ee4c041dfcbed51ffb4c422309cc8c1ad8dfa
Loan <lfrancois@velan.fr>
2025-12-15 14:35:02 +0100

    fix: sacha sais pas ecrire

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class HereBro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'HereBrp',
+      title: 'HereBro',
       themeMode: ThemeMode.dark,
       darkTheme: AppTheme.darkTheme,
       routerConfig: appRouter,
